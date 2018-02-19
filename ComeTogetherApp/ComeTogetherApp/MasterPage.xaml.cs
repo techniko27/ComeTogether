0f85250420ec10aed2c8877d37dca8b23a9a44ba
techniko27 <nikoscheuermann@web.de>
2018-02-19 14:08:53 +0100

    Sidebar menu and Eventspage added
    MainPage and MasterPage for Sidebar menu added
    Icons for Sidebar
    Eventspage added

---
+++ ComeTogetherApp/ComeTogetherApp/MasterPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ComeTogetherApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : ContentPage
+    {
+        public ListView ListView { get { return listView; } }
+        public String listColor;
+
+        public MasterPage()
+        {
+            InitializeComponent();
+
+            listView.BackgroundColor = Color.FromHex(App.GetMenueColor());
+
+            var masterPageItems = new List<MasterPageItem>();
+
+            masterPageItems.Add(new MasterPageItem
+            {
+                Title = "Events",
+                IconSource = "rooms.png",
+                TargetType = typeof(EventsPage)
+            });
+            masterPageItems.Add(new MasterPageItem
+            {
+                Title = "Settings",
+                IconSource = "todo.png",
+                TargetType = typeof(EventsPage)
+            });
+            /*
+            masterPageItems.Add(new MasterPageItem
+            {
+                Title = "lo",
+                //IconSource = "contacts.png",
+                TargetType = typeof(RoomPage),
+            });
+            */
+            masterPageItems.Add(new MasterPageItem
+            {
+                //Title = App.GetUsername(),
+                Title = "Account",
+                IconSource = "contacts.png",
+                TargetType = typeof(EventsPage)
+            });
+            masterPageItems.Add(new MasterPageItem
+            {
+                Title = "LogOff",
+                IconSource = "logoff.png",
+                TargetType = typeof(LoginPage)
+            });
+
+            listView.ItemsSource = masterPageItems;
+        }
+    }
+}
