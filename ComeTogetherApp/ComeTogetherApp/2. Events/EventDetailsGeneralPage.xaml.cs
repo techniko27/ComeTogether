6e353c5f621bd3dc2fb391e3bebabdf57e9b2e16
Kahmul <kahmul78@web.de>
2018-02-28 12:23:50 +0100

    Add initial single event detail tab view

---
+++ ComeTogetherApp/ComeTogetherApp/2. Events/EventDetailsGeneralPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ComeTogetherApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EventDetailsGeneralPage : ContentPage
+    {
+        private ScrollView scrollView;
+
+        public EventDetailsGeneralPage()
+        {
+            InitializeComponent();
+
+            Title = "Allgemein";
+
+            scrollView = new ScrollView();
+            StackLayout stackLayout = createStackLayout();
+
+            scrollView.Content = stackLayout;
+
+            Content = scrollView;
+        }
+
+        private StackLayout createStackLayout()
+        {
+            StackLayout stackLayout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Start,
+                Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5)
+            };
+
+            StackLayout overviewInfoLayout = createOverviewInfoLayout();
+            StackLayout buttonOptionsLayout = createButtonOptionsLayout();
+
+            stackLayout.Children.Add(overviewInfoLayout);
+            stackLayout.Children.Add(buttonOptionsLayout);
+
+            return stackLayout;
+        }
+
+
+        private StackLayout createOverviewInfoLayout()
+        {
+            StackLayout overviewInfoLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5)
+            };
+
+            return overviewInfoLayout;
+        }
+
+
+        private StackLayout createButtonOptionsLayout()
+        {
+            StackLayout buttonOptionsLayout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Start,
+                Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5)
+            };
+
+            Button inviteFriendsButton = new Button
+            {
+                Text = "Freunde einladen"
+            };
+            Button leaveEventButton = new Button
+            {
+                Text = "Veranstaltung verlassen"
+            };
+
+            buttonOptionsLayout.Children.Add(inviteFriendsButton);
+            buttonOptionsLayout.Children.Add(leaveEventButton);
+
+            return buttonOptionsLayout;
+        }
+    }
+}
---
+++ ComeTogetherApp/ComeTogetherApp/2. Events/EventDetailsToDosPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ComeTogetherApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EventDetailsToDosPage : ContentPage
+    {
+        public EventDetailsToDosPage()
+        {
+            InitializeComponent();
+
+            Title = "ToDos";
+        }
+    }
+}
