078881742ffe9a4d862382b94c15dbc2eed1d3e2
techniko27 <nikoscheuermann@web.de>
2018-02-28 11:04:56 +0100

    First EventButton on EventPage
    Loop for building EventButtons automated from Serverdata

--- ComeTogetherApp/ComeTogetherApp/2. Events/EventsPage.xaml.cs
+++ ComeTogetherApp/ComeTogetherApp/2. Events/EventsPage.xaml.cs
@@ -19,6 +19,8 @@ namespace ComeTogetherApp
 
         private ScrollView scroll;
         private StackLayout stack;
+        private StackLayout stackButton;
+        private Grid grid;
         private ActivityIndicator activityIndicator;
 
         public EventsPage()
@@ -43,12 +45,83 @@ namespace ComeTogetherApp
 
             stack = new StackLayout
             {
-                VerticalOptions = LayoutOptions.Start,
-                Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5)
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Padding = new Thickness(2, 2, 2, 2)
             };
             stack.Children.Add(searchBar);
             stack.Children.Add(resultsLabel);
+
             scroll.Content = stack;
+
+            grid = new Grid()
+            {
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                ColumnSpacing = 6,
+                RowSpacing = 6
+            };
+            stack.Children.Add(grid);
+
+            /*
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(250, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(250, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(150, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(150, GridUnitType.Star) });
+
+            var topLeft = new Button() { Text = "Top Left" };
+            var topRight = new Button() { Text = "Top Right" };
+            var bottomLeft = new Button() { Text = "Bottom Left" };
+            var bottomRight = new Button() { Text = "Bottom Right" };
+
+            grid.Children.Add(topLeft, 0, 0);
+            grid.Children.Add(topRight, 0, 1);
+            grid.Children.Add(bottomLeft, 1, 0);
+            grid.Children.Add(bottomRight, 1, 1);
+            */
+            
+            for (int i = 0; i < 4; i++)
+            {
+                //grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(150, GridUnitType.Star) });
+                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(230, GridUnitType.Star) });
+
+                for (int j = 0; j < 2; j++)
+                {
+                    var eventImage = new Image { Aspect = Aspect.AspectFit };
+                    eventImage.Source = "info.png";
+                    Label eventNameLabel = new Label
+                    {
+                        Text = "Eventname",
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        FontSize = 15
+                    };
+                    Label eventMembercountLabel = new Label
+                    {
+                        Text = "EventMemberCount",
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        FontSize = 15
+                    };
+                    var tapGestureRecognizer = new TapGestureRecognizer();
+                    tapGestureRecognizer.Tapped += (object sender, EventArgs e) =>
+                    {
+                        // handle the tap
+                        OnEventClicked(sender, e);
+                    };
+                    stackButton = new StackLayout
+                    {
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                        Padding = new Thickness(2, 2, 2, 2),
+                        BackgroundColor = Color.FromHex("41BAC1")
+                        
+                    };
+                    stackButton.GestureRecognizers.Add(tapGestureRecognizer);
+                    stackButton.Children.Add(eventImage);
+                    stackButton.Children.Add(eventNameLabel);
+                    stackButton.Children.Add(eventMembercountLabel);
+
+                    grid.Children.Add(stackButton, j, i);
+                }
+            }
 
             activityIndicator = new ActivityIndicator()
             {
@@ -59,7 +132,7 @@ namespace ComeTogetherApp
                 VerticalOptions = LayoutOptions.CenterAndExpand,
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
             };
-            Content = scroll;
+            Content = activityIndicator;
 
             ServerGetEvents();
         }
@@ -76,12 +149,20 @@ namespace ComeTogetherApp
                     System.Diagnostics.Debug.WriteLine($"{e.Key} is {e.Object.Name}");
                     resultsLabel.Text = e.Key;
                 }
+
             }
             catch (Exception e)
             {
                 await DisplayAlert("Server connection failure", "Communication problems occured while querying", "OK");
                 System.Diagnostics.Debug.WriteLine(e);
             }
+
+            activityIndicatorSwitch();
+        }
+
+        void OnEventClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Event", "Klicked", "OK");
         }
 
         private void activityIndicatorSwitch()
