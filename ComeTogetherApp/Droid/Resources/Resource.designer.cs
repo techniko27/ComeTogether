a58a64f48cfa3042bad6886ebbf0e055b039dc4c
Kahmul <kahmul78@web.de>
2018-04-19 10:38:30 +0200

    Add save images to project

--- ComeTogetherApp/Droid/Resources/Resource.designer.cs
+++ ComeTogetherApp/Droid/Resources/Resource.designer.cs
@@ -2336,8 +2336,26 @@ namespace ComeTogetherApp.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
+			// aapt resource value: 0x7f02014f
+			public const int avd_hide_password_1 = 2130837839;
+			
+			// aapt resource value: 0x7f020150
+			public const int avd_hide_password_2 = 2130837840;
+			
+			// aapt resource value: 0x7f020151
+			public const int avd_hide_password_3 = 2130837841;
+			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
+			
+			// aapt resource value: 0x7f020152
+			public const int avd_show_password_1 = 2130837842;
+			
+			// aapt resource value: 0x7f020153
+			public const int avd_show_password_2 = 2130837843;
+			
+			// aapt resource value: 0x7f020154
+			public const int avd_show_password_3 = 2130837844;
 			
 			// aapt resource value: 0x7f020055
 			public const int benutzer = 2130837589;
